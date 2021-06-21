38302c43717013ecd53d193fa8b4bd7642167240
sarafraghav <40730714+sarafraghav@users.noreply.github.com>
2021-06-21 08:38:43 +0530

    Update settings.py

--- agency/settings.py
+++ agency/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com','18.136.212.110']
+ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com','18.136.212.110','127.0.0.1']
 
 
 # Application definition
