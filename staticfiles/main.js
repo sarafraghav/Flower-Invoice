1e101a2ea964dd751b33d4f91d82dfb0eb736904
sarafraghav <40730714+sarafraghav@users.noreply.github.com>
2021-06-21 08:38:14 +0530

    c

---
+++ staticfiles/main.js
@@ -0,0 +1,30 @@
+console.log("Sanity check!");
+var x = window.location.href.match(/\/([^\/]+)\/?$/)[1];
+var y = window.location.href.match(/([^\/]+)\/[^\/]+\/[^\/]+\/?$/)[1];
+console.log(y)
+
+// Get Stripe publishable key
+fetch("/config/"+x)
+.then((result) => { return result.json(); })
+.then((data) => {
+  console.log(data);
+  
+  // Initialize Stripe.js;
+  const stripe = Stripe(data.publicKey, {stripeAccount: data.stripe_account});
+
+  // new
+  // Event handler
+  document.querySelector("#submitBtn").addEventListener("click", () => {
+    // Get Checkout Session ID
+    fetch("/create-checkout-session/"+x+"/"+y)
+    .then((result) => { return result.json(); })
+    .then((data) => {
+      console.log(data);
+      // Redirect to Stripe Checkout
+      return stripe.redirectToCheckout({sessionId: data.sessionId})
+    })
+    .then((res) => {
+      console.log(res);
+    });
+  });
+});
---
+++ staticfiles/prod.js
@@ -0,0 +1,26 @@
+console.log("Sanity check!");
+var x = window.location.href.match(/\/([^\/]+)\/?$/)[1];
+// Get Stripe publishable key
+console.log(x);
+fetch("/pconfig/"+x)
+.then((result) => { return result.json(); })
+.then((data) => {
+  // Initialize Stripe.js;
+  const stripe = Stripe(data.publicKey, {stripeAccount: data.stripe_account});
+
+  // new
+  // Event handler
+  document.querySelector("#submitBtn").addEventListener("click", () => {
+    // Get Checkout Session ID
+    fetch("/pcreate-checkout-session/"+x)
+    .then((result) => { return result.json(); })
+    .then((data) => {
+      console.log(data);
+      // Redirect to Stripe Checkout
+      return stripe.redirectToCheckout({sessionId: data.sessionId})
+    })
+    .then((res) => {
+      console.log(res);
+    });
+  });
+});
