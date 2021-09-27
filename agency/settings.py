97f351fbd303478bd86f0650943702a0d3a0e2dc
sarafraghav <40730714+sarafraghav@users.noreply.github.com>
2021-09-27 23:49:57 +0530

    djcelery

--- agency/settings.py
+++ agency/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'buy',
+    'djcelery',
     'payments',
     'pyuploadcare.dj',
 ]
@@ -163,7 +164,7 @@ EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'sarafraghav1@gmail.com'
 
 BROKER_URL = 'amqp://fcqqjcby:wrsZFkntXCUj7X8WXm1C0zf_CU5258aN@hornet.rmq.cloudamqp.com/fcqqjcby'
-CELERY_RESULT_BACKEND = 'amqp://fcqqjcby:wrsZFkntXCUj7X8WXm1C0zf_CU5258aN@hornet.rmq.cloudamqp.com/fcqqjcby'
+CELERY_RESULT_BACKEND = 'djcelery.backends.database:DatabaseBackend'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
