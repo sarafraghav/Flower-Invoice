85b6b1083cf4cade01305b17d39c8a2b563d5a63
Raghav Saraf <40730714+sarafraghav@users.noreply.github.com>
2021-11-30 14:27:25 +0530

    Email SMTP

--- agency/settings.py
+++ agency/settings.py
@@ -158,9 +158,9 @@ UPLOADCARE = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  
 MAILER_EMAIL_BACKEND = EMAIL_BACKEND  
-EMAIL_HOST = 'smtp.sendgrid.net'  
+EMAIL_HOST = 'smtp-relay.sendinblue.com'  
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_USER = 'sarafraghav4@gmail.com'
 EMAIL_PORT = 587  
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'sarafraghav1@gmail.com'
